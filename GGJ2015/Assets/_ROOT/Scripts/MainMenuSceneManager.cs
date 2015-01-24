79dab0bf306c3d3427f52575c12bbcd737d614da
DoomSamurai <math.chenard@gmail.com>
2015-01-24 00:04:33 -0500

    Can now launch a server and enter the game scene

---
+++ GGJ2015/Assets/_ROOT/Scripts/MainMenuSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kathulhu;
+
+public class MainMenuSceneManager : SceneManager {
+
+
+
+    public void StartGame()
+    {
+        BoltLauncher.StartServer( UdpKit.UdpEndPoint.Parse( "127.0.0.1:27000" ) );
+
+        GameController.LoadScene( "Game" );
+    }
+
+}
