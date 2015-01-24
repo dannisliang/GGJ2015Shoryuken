230d9b7d3de2d4f175e601ff8e4433c62e59f109
DELASSUS Benoit <126354@supinfo.com>
2015-01-24 06:23:17 -0500

    enemy spawn 2

---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/SpawnEnemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	public int numberToSpawn;
+	public GameObject enemyPrefab;
+	public EnemyState stateSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TriggerAction(GameObject collider)
+	{
+		for(int i = 0; i<numberToSpawn; i++)
+		{
+			GameObject obj = (GameObject)GameObject.Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+			EnemyAi enemy  = obj.GetComponent<EnemyAi>();
+			enemy.state = stateSpawn;
+			enemy.target = collider;
+		}
+	}
+}
---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/TriggerPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPlayer : MonoBehaviour {
+
+	public GameObject action;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.name == "Player")
+			action.SendMessage("TriggerAction", other.gameObject, SendMessageOptions.DontRequireReceiver);
+	}
+}
