799fedf9407d9bab4ac010d27fab6f1ab3e2d22f
DoomSamurai <math.chenard@gmail.com>
2015-01-24 02:02:20 -0500

    Standalone bolt callbacks example

--- GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/StandaloneBoltCallbacks.cs
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/StandaloneBoltCallbacks.cs
@@ -1,28 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Bolt;
 
-[BoltGlobalBehaviour(BoltNetworkModes.Server)]
-public class StandaloneBoltCallbacks : GlobalEventListener {
+[BoltGlobalBehaviour( BoltNetworkModes.Server )]
+public class StandaloneBoltCallbacks : GlobalEventListener
+{
 
     public override void BoltStarted()
     {
         base.BoltStarted();
 
-        Debug.Log("Bolt:BoltStarted");
+        Debug.Log( "Bolt:BoltStarted" );
     }
 
     public override void Connected( BoltConnection connection )
     {
         base.Connected( connection );
 
-        Debug.Log("Bolt:Connected");
+        logMessages.Insert( 0, string.Format( "{0} connected", connection.RemoteEndPoint ) );
     }
 
-    public override void ConnectRequest( UdpKit.UdpEndPoint endpoint )
-    {
-        base.ConnectRequest( endpoint );
+    List<string> logMessages = new List<string>();
 
-        Debug.Log( "Bolt:ConnectRequest" );
-    }
+    //void OnGUI()
+    //{
+    //    // only display max the 5 latest log messages
+    //    int maxMessages = Mathf.Min( 5, logMessages.Count );
+
+    //    GUILayout.BeginArea( new Rect( Screen.width / 2 - 200, Screen.height - 100, 400, 100 ), GUI.skin.box );
+
+    //    for ( int i = 0; i < maxMessages; ++i )
+    //    {
+    //        GUILayout.Label( logMessages[i] );
+    //    }
+
+    //    GUILayout.EndArea();
+    //}
+
+
+    //"Listen to event" example
+    //public override void OnEvent( ShowDebugMessage evnt )
+    //{
+    //    logMessages.Insert( 0, evnt.message );
+    //}
+
+    //"Raise event" example
+    //var log = ShowDebugMessage.Create();
+    //log.message = string.Format( "{0} connected", connection.RemoteEndPoint );
+    //log.Send();
 }
