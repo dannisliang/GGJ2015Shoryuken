fbccc9a01bdc400676043decccc6812d786c2988
Rod Game <RodGamerz@gmail.com>
2015-01-24 04:24:31 -0500

    Radar script + Project setting to build android + ignore *.apk

---
+++ GGJ2015/Assets/_ROOT/Scripts/_RADAR/ButtonHackManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class ButtonHackManager : MonoBehaviour, IPointerClickHandler {
+
+    public GameObject m_contextMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+    }
+}
