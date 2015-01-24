48b7025833eebf190e4aedbd3f61bbdf2b738d54
DELASSUS Benoit <126354@supinfo.com>
2015-01-24 02:36:37 -0500

    scene game start pathfinding

---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/EnemyAi.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class EnemyAi : MonoBehaviour {
+
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		Seeker seeker = GetComponent<Seeker>();
+		seeker.StartPath (transform.position, transform.position+transform.forward*speed, OnPathComplete);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(GameObject.Find("Player").transform.position);
+	}
+
+	public void OnPathComplete (Path p) {
+		//We got our path back
+		if (p.error) {
+			//Nooo, a valid path couldn't be found
+		} else {
+			//Yey, now we can get a Vector3 representation of the path
+			//from p.vectorPath
+		}
+	}
+}
