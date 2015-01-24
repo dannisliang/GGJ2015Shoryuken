0b43a66f7765c097cb79a9c420c740454ab907ce
DoomSamurai <math.chenard@gmail.com>
2015-01-23 23:48:23 -0500

    Automatically load MainMenu
    also fixed missing reference in loading screen

---
+++ GGJ2015/Assets/_ROOT/Scripts/AutoLoadMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoLoadMainMenu : MonoBehaviour {
+	
+	void Start () {
+        Kathulhu.GameController.LoadScene( "MainMenu" );
+	}
+	
+}
