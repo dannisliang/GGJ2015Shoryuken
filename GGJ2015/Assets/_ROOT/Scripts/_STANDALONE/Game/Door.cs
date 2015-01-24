e10e6a9f30e4c468a1f0d7bc2b9b523184ae31ca
DELASSUS Benoit <126354@supinfo.com>
2015-01-24 16:05:25 -0500

    door system

--- GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/Door.cs
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/Door.cs
@@ -6,6 +6,12 @@ public class Door : InteractableObject {
     [SerializeField]
     public bool Locked = true;
 
+	public Vector3 openPosition;
+	public Vector3 closePosition;
+	private bool opening;
+
+	public Renderer doorRenderer;
+
     public void Unlock()
     {
         if ( Locked == true )
@@ -17,5 +23,79 @@ public class Door : InteractableObject {
             //etc.
         }
     }
+
+	float time = 0;
+	public void Update()
+	{
+		if(Locked)
+		{
+			doorRenderer.material.SetColor("_OutlineColor", Color.red);
+		}
+		else
+			doorRenderer.material.SetColor("_OutlineColor", Color.green);
+
+		if(opening)
+		{
+			MoveTowardsTarget(openPosition);
+		}
+		else
+		{
+			MoveTowardsTarget(closePosition);
+		}
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(!Locked)
+			{
+				opening = true;
+			}
+		}
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(!Locked)
+			{
+				opening = true;
+			}
+		}
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			if(!Locked)
+			{
+				opening = false;
+			}
+		}
+	}
+
+	private void MoveTowardsTarget(Vector3 targetPosition) {
+		//the speed, in units per second, we want to move towards the target
+		float speed = 1;
+		//move towards the center of the world (or where ever you like)
+		
+		Vector3 currentPosition = this.transform.position;
+		//first, check to see if we're close enough to the target
+		if(Vector3.Distance(currentPosition, targetPosition) > .1f) { 
+			Vector3 directionOfTravel = targetPosition - currentPosition;
+			//now normalize the direction, since we only want the direction information
+			directionOfTravel.Normalize();
+			//scale the movement on each axis by the directionOfTravel vector components
+			
+			this.transform.Translate(
+				(directionOfTravel.x * speed * Time.deltaTime),
+				(directionOfTravel.y * speed * Time.deltaTime),
+				(directionOfTravel.z * speed * Time.deltaTime),
+				Space.World);
+		}
+	}
 
 }
