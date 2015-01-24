62784bed4d81bd2522de21ba3d2166abd8097317
DoomSamurai <math.chenard@gmail.com>
2015-01-24 04:11:07 -0500

    Single AppStart scene

---
+++ GGJ2015/Assets/_ROOT/Scripts/AppInit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kathulhu;
+
+public class AppInit : MonoBehaviour 
+{
+
+    public bool host = true;
+
+	
+	void Start () {
+
+#if UNITY_ANDROID
+        GameController.LoadScene( "MainMenuRadar" );
+#else
+        GameController.LoadScene( host ? "MainMenu" : "MainMenuRadar" );
+#endif
+
+	}
+
+}
