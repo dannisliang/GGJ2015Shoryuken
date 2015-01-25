040f9d3a126272c4da3be72859b651396167a2ed
MikeGonthier <mgonth@gmail.com>
2015-01-25 05:25:31 -0500

    Add script on fx_bullet

---
+++ GGJ2015/Assets/FX/fx_bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fx_bullet : MonoBehaviour {
+
+	private Light myLight;
+	private float mySpeed;
+
+	public void Flash(float speed){
+		mySpeed = speed;
+		myLight = this.GetComponent<Light>();
+		myLight.intensity = 1;
+		StartCoroutine ("Anim");
+	}
+	
+	IEnumerator Anim(){
+		while(myLight.intensity > 0){
+			myLight.intensity = Mathf.Lerp(1,0,Time.deltaTime * mySpeed);
+			yield return null;
+		}
+	}
+
+}
