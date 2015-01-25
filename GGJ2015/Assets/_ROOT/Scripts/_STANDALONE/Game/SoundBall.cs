007daecaa2e4c2d9a78de5884ab1b1426900d9fa
DELASSUS Benoit <126354@supinfo.com>
2015-01-24 22:20:55 -0500

    Weapon begin

---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/SoundBall.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundBall : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Enemy")
+		{
+			EnemyAi enemy = other.GetComponent<EnemyAi>();
+			enemy.state = EnemyState.Flee;
+			enemy.RevengePoint = Player.transform;
+		}
+	}
+}
