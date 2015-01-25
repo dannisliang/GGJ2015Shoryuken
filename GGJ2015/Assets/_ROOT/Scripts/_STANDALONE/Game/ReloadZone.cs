40aafd96b5311e6fbb7458d6544118518fa1302e
DELASSUS Benoit <126354@supinfo.com>
2015-01-25 05:19:26 -0500

    reloadZone

---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/ReloadZone.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadZone : MonoBehaviour {
+	
+	public bool inZone;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(inZone)
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				GameManager.Instance.PlayerInstance.GetComponentInChildren<SoundRifle>().munitions = 3;
+			}
+		}
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			inZone = true;
+		}
+	}
+
+	public void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			inZone = false;
+		}
+	}
+}
