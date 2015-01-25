8d08236534703b15df5185626e7ed3091eaf2094
DoomSamurai <math.chenard@gmail.com>
2015-01-25 14:50:10 -0500

    various

--- GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/EnemyAi.cs
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/EnemyAi.cs
@@ -176,9 +176,9 @@ public class EnemyAi : MonoBehaviour {
 
 	private void PingPosition()
 	{
-		//PingMonsterPosition ping = PingMonsterPosition.Create();
-		//ping.Position = transform.position;
-		//ping.Send();
+        PingMonsterPosition ping = PingMonsterPosition.Create();
+        ping.Position = transform.position;
+        ping.Send();
 	}
 
 	public Seeker Seeker {
---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/PlayerPositionPing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPositionPing : MonoBehaviour {
+
+	
+	IEnumerator Start () {
+        yield return new WaitForSeconds( 0.5f );
+
+        SetPlayerPosition ping = SetPlayerPosition.Create();
+        ping.WorldPosition = transform.position;
+        ping.Send();
+	
+	}
+	
+}
