b3d275bce5decb3f64227f35bfbb150e2cabf46f
DoomSamurai <math.chenard@gmail.com>
2015-01-25 13:50:12 -0500

    RoomManagerScript

---
+++ GGJ2015/Assets/_ROOT/Scripts/_STANDALONE/Game/RoomManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(BoxCollider))]
+public class RoomManager : MonoBehaviour {
+
+    [SerializeField]
+    private List<InteractableObject> _hiddenInteractableObjects;
+
+    void Awake(){
+        if ( _hiddenInteractableObjects == null )
+            _hiddenInteractableObjects = new List<InteractableObject>();
+    }
+
+    void OnTriggerEnter( Collider other )
+    {
+        if ( other.tag == "Player" )
+            SetInteractablesVisibility(true);
+    }
+
+    void OnTriggerExit( Collider other )
+    {
+        if ( other.tag == "Player" )
+            SetInteractablesVisibility(false);
+    }
+
+    void SetInteractablesVisibility(bool value)
+    {
+        foreach ( var item in _hiddenInteractableObjects )
+        {
+            SetInteractableIconVisibility evt = SetInteractableIconVisibility.Create();
+            evt.InteractableIdentifier = item.Identifier;
+            evt.Visible = value;
+        }
+    }
+
+}
