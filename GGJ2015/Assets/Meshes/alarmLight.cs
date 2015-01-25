3dbbe1240ac9ea2e2d4e832c69dceb88a0566056
MikeGonthier <mgonth@gmail.com>
2015-01-25 05:58:07 -0500

    added alarmLight

---
+++ GGJ2015/Assets/Meshes/alarmLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class alarmLight : MonoBehaviour {
+
+	float speed = 4;
+	Light myLight;
+
+	void Start(){
+		myLight = GetComponent<Light> ();
+		Play ();
+	}
+
+	public void Play(){
+		StartCoroutine ("Rotate");
+	}
+
+	public void Stop(){
+		StopCoroutine ("Rotate");
+	}
+
+	IEnumerator Rotate(){
+		while (true) {
+			myLight.transform.Rotate(Vector3.up,speed);	
+			yield return null;
+		}
+	}
+
+}
